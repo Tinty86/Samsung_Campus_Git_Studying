5f0e705bc7dad0bd4fac17e6a4c3b23e49bbf633
Tinty <vladimirhomutov55917@gmail.com>
2024-11-09 13:59:17 +0300

    Added for

--- src/com/examlpe/test/App.java
+++ src/com/examlpe/test/App.java
@@ -2,6 +2,9 @@ package com.examlpe.test;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello Git!!!");
+        for (int i = 0; i<10; i++){
+            System.out.println(i);
+        }
+
     }
 }
